600442f5f7d64b250929ec08b24ed7b5918ef00c
prakash18399 <prakashchoudhary1831999@gmail.com>
2021-05-16 22:33:12 +0530

    changed deploy stuffs

--- CCL/settings.py
+++ CCL/settings.py
@@ -29,7 +29,7 @@ DEBUG = False
 
 ALLOWED_HOSTS = [‘localhost’,
 ‘127.0.0.1’,
-'https://cloud-file-upload-and-download.herokuapp.com']
+'cloud-file-upload-and-download.herokuapp.com']
 
 
 # Application definition
---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn CCL.wsgi --log-file -
