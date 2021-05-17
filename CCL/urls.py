308f9d66929cdf45fee9941d19b163ff9ff486ab
prakash18399 <prakashchoudhary1831999@gmail.com>
2021-05-17 09:27:31 +0530

    ,

--- CCL/urls.py
+++ CCL/urls.py
@@ -28,7 +28,11 @@ urlpatterns = [
     path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
     path('', include('blog.urls')),
 ]
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) \
+        + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
+#urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 #if settings.DEBUG:
 #    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
